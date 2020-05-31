ac4a0fb27acbcaa9d73df31777d3e0a4b1290c98
vanduc2514 <vanduc2514@gmail.com>
2020-05-31 16:44:21 +0100

    Refactor

--- src/main/java/config/AppConfigurationThymeleaf.java
+++ src/main/java/config/AppConfigurationThymeleaf.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -14,8 +13,6 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import service.IProductService;
 import service.ProductService;
-
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebMvc
@@ -35,7 +32,7 @@ public class AppConfigurationThymeleaf implements ApplicationContextAware {
         templateResolver.setPrefix("/views/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
     }
 
@@ -50,7 +47,7 @@ public class AppConfigurationThymeleaf implements ApplicationContextAware {
     public ThymeleafViewResolver viewResolver() {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
-        viewResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        viewResolver.setCharacterEncoding("UTF-8");
         return viewResolver;
     }
 
--- src/main/java/config/AppInitializer.java
+++ src/main/java/config/AppInitializer.java
@@ -2,7 +2,6 @@ package config;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
 import javax.servlet.Filter;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
